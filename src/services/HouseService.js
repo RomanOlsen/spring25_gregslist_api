a79a37a803799191dfd8b9f7f89b4e6d0334712b
RomanOlsen <romanjolsen@gmail.com>
2025-03-11 16:39:51 -0600

    pages

--- src/services/HouseService.js
+++ src/services/HouseService.js
@@ -2,22 +2,41 @@ import { dbContext } from "../db/DbContext.js"
 
 class HouseService {
 
-  async getSecretSearch() {
-    const houses = await dbContext.Houses.find({ bedrooms: 5 })
-    return houses
-  }
-  async getCertainKindsOfHouses(queries) {
+  async getCertainKindsOfHouses(queries) { // bad name of function. since it could just get all houses but only with pageNumber as a query. getHousesByQuery is likely better
     const arrangement = queries.sort
     delete queries.sort
+    
+    let pageNumber = queries.page
+    delete queries.page
 
-
+    if (pageNumber == null) {
+      pageNumber = 'No page specified'
+    }
+    
     const allHouses = await dbContext.Houses.countDocuments()
-    // const houses = await dbContext.Houses.find(queries)
     const houses = await dbContext.Houses
-      .find(queries)
-      .sort(arrangement) // only works if we specify to sort in get request
-    return houses
+    .find(queries)
+    .sort(arrangement) // only works if we specify to sort in get request
+
+    const output = {
+      message: 'heres your request',
+      page: pageNumber,
+      requestedHouses: houses,
+    }
+
+
+    return output
   }
+  
+  // SECTION //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  
+  async getSecretSearch() {
+    const secretHouses = await dbContext.Houses.find({ bedrooms: 5 })
+    return secretHouses
+  }
+  
+
   async getHousebyID(requestedHouse) {
     const foundHouse = await dbContext.Houses.findById(requestedHouse)
     return foundHouse
@@ -25,7 +44,12 @@ class HouseService {
   }
   async getHouses() {
     const houses = await dbContext.Houses.find()
-    return houses
+    const houseStats = {
+      message: 'Here they are!',
+      totalHouses: await dbContext.Houses.countDocuments(),
+      houses: houses
+    }
+    return houseStats
   }
 }
 
